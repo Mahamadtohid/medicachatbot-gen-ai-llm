e658248366f5bcd184bb3de1c9ebfafc55dd07e5
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2024-12-11 12:47:29 +0530

    First Uplod

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical AI Chatbot',
+    version='0.0.0',
+    author='Mahamadtohid Naikwadi',
+    author_email='naikwadimdtohid@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
